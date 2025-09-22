3d1118bb8913823bc1400b56e391da53465472ef
Your Name <you@example.com>
2025-09-22 14:09:30 +0000

    first commit

--- Linux/project/tutorial/action_script/main.cpp
+++ Linux/project/tutorial/action_script/main.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <libgen.h>
+#include <signal.h>
 
 #include "Camera.h"
 #include "Point.h"
@@ -19,52 +20,78 @@
 #include "Action.h"
 #include "Head.h"
 #include "Walking.h"
-#include "MX28.h"
 #include "MotionManager.h"
 #include "LinuxMotionTimer.h"
-#include "LinuxCM730.h"
 #include "LinuxActionScript.h"
+#include "LinuxDARwIn.h"
 
-#ifdef MX28_1024
-#define MOTION_FILE_PATH    "../../../../Data/motion_1024.bin"
-#else
 #define MOTION_FILE_PATH    "../../../../Data/motion_4096.bin"
-#endif
 
 #define INI_FILE_PATH       "../../../../Data/config.ini"
+
+
+LinuxArbotixPro linux_arbotixpro("/dev/ttyUSB0");
+ArbotixPro arbotixpro(&linux_arbotixpro);
+LinuxMotionTimer linuxMotionTimer;
+
 
 void change_current_dir()
 {
     char exepath[1024] = {0};
-    if(readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
+    if (readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
         chdir(dirname(exepath));
 }
 
+void sighandler(int sig)
+{
+    struct termios term;
+    tcgetattr( STDIN_FILENO, &term );
+    term.c_lflag |= ICANON | ECHO;
+    tcsetattr( STDIN_FILENO, TCSANOW, &term );
+
+    exit(0);
+}
 int main(void)
 {
-    printf( "\n===== Action script Tutorial for DARwIn =====\n\n");
+
+    signal(SIGABRT, &sighandler);
+    signal(SIGTERM, &sighandler);
+    signal(SIGQUIT, &sighandler);
+    signal(SIGINT, &sighandler);
+
+    char filename[128];
 
     minIni* ini = new minIni(INI_FILE_PATH);
 
     change_current_dir();
+        strcpy(filename, MOTION_FILE_PATH); // Set default motion file path
 
-    Action::GetInstance()->LoadFile(MOTION_FILE_PATH);
-
-    //////////////////// Framework Initialize ////////////////////////////
-    LinuxCM730 linux_cm730("/dev/ttyUSB0");
-    CM730 cm730(&linux_cm730);
-    if(MotionManager::GetInstance()->Initialize(&cm730) == false)
+    /////////////// Load/Create Action File //////////////////
+    if (Action::GetInstance()->LoadFile(filename) == false)
     {
-        printf("Fail to initialize Motion Manager!\n");
-            return 0;
+        printf("Can not open %s\n", filename);
+        exit(0);
+    }
+    ////////////////////////////////////////////////////////////
+
+    //    PS3Controller_Start();
+    //////////////////// Framework Initialize ////////////////////////////
+    if (MotionManager::GetInstance()->Initialize(&arbotixpro) == false)
+    {
+        printf("Initializing Motion Manager failed!\n");
+        exit(0);
     }
     MotionManager::GetInstance()->LoadINISettings(ini);
+    MotionManager::GetInstance()->SetEnable(false);
     MotionManager::GetInstance()->AddModule((MotionModule*)Action::GetInstance());
-    LinuxMotionTimer *motion_timer = new LinuxMotionTimer(MotionManager::GetInstance());
-    motion_timer->Start();
+    linuxMotionTimer.Initialize(MotionManager::GetInstance());
+    linuxMotionTimer.Stop();
     /////////////////////////////////////////////////////////////////////
+   
 
+    Action::GetInstance()->m_Joint.SetEnable(true,true);
     MotionManager::GetInstance()->SetEnable(true);
+    linuxMotionTimer.Start();
 
     Action::GetInstance()->Start(1);    /* Init(stand up) pose */
     while(Action::GetInstance()->IsRunning()) usleep(8*1000);
